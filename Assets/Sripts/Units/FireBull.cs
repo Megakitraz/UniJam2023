12eeff17827b5e0b955b3e468182b3659b3257cf
TiboHrqn <harquin.thibault@gmail.com>
2023-11-25 14:17:41 +0100

    yoooo

---
+++ Assets/Sripts/Units/FireBull.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBull : Unit
+{
+    
+    private bool isEnraged = false;
+    [SerializeField] private Direction direction;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Tick()
+    {
+        if (isEnraged)
+        {
+            // TODO move fireBull
+            return;
+        }
+        CheckPlayerVisibility();
+    }
+
+    protected override void ApplyEffectOnNeighbor()
+    {
+        var neighbors= tileGrid.GetNeighborsFor(tileCoord);
+        foreach (var neighbor in neighbors)
+        {
+            Tile tile = tileGrid.GetTileAt(neighbor);
+            //TODO tile.ApplyHeat();
+        }
+    }
+
+    private void CheckPlayerVisibility()
+    {
+        throw new System.NotImplementedException();
+        // TODO implement test to see if player is visible by the bull, and set facing dir accordingly
+    }
+
+    private bool IsPlayerVisible()
+    {
+        Vector3Int coords = tileCoord;
+        bool solved = false;
+        while(!solved)
+        {
+            switch(direction)
+            {
+            case Direction.up:
+                coords += new Vector3Int(0,0,1);
+                break;
+
+            case Direction.right:
+                coords += new Vector3Int(1,0,0);
+                break;
+
+            case Direction.down:
+                coords += new Vector3Int(0,0,-1);
+                break;
+
+            case Direction.left:
+                coords += new Vector3Int(-1,0,0);
+                break;
+
+            default:
+                Debug.Log(":(");
+            break;
+            }
+            Tile tile = TileGrid.Instance.GetTileAt(coords);
+            if (tile != null)
+            {
+                if (tile.IsPlayerOnTile())
+                {
+                    return true;
+                }
+                if (!tile.IsReachable())
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+
+
+    public enum Direction
+    {
+        up,
+        right,
+        down,
+        left
+    }
+    
+    
+}
